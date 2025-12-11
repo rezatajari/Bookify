ef77ccc9f72fb46462c0576e59734c083c8b9ba7
rezatajari <reza.tajari70@gmail.com>
2025-12-11 20:11:13 +0330

    feat: create base entity for guid

--- Domain/Apartments/Apartment.cs
+++ Domain/Apartments/Apartment.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,9 +7,13 @@ using System.Threading.Tasks;
 
 namespace Domain.Apartments
 {
-    public sealed class Apartment
+    public sealed class Apartment :Entity
     {
-        public Guid Id { get; private set; }
+        public Apartment(Guid id)
+            :base(id)
+        {
+            
+        }
         public string Name { get; private set; }
         public string Description { get; private set; }
         public string Country { get; private set; }
