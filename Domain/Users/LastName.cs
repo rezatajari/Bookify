c27d3dd2b01d3cc7341e3ed21073fb37bbeb6b7c
rezatajari <reza.tajari70@gmail.com>
2025-12-12 12:52:27 +0330

    feat: user entity

---
+++ Domain/Users/LastName.cs
@@ -0,0 +1,4 @@
+﻿namespace Domain.Users
+{
+    public record LastName(string Value);
+}
---
+++ Domain/Users/User.cs
@@ -0,0 +1,30 @@
+﻿using Domain.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Users
+{
+    public sealed class User : Entity
+    {
+        private User(Guid id, FirstName firstName, LastName lastName, Email email)
+            : base(id)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+        }
+
+        public FirstName FirstName { get; private set; }
+        public LastName LastName { get; private set; }
+        public Email Email { get; private set; }
+
+        public static User Create(FirstName firstName, LastName lastName, Email email)
+        {
+            var user= new User(Guid.NewGuid(), firstName, lastName, email);
+            return user;
+        }
+    }
+}
