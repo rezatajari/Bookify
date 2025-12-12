d347a73d7f87097b803e56171c0fdd4f7084324f
rezatajari <reza.tajari70@gmail.com>
2025-12-12 13:39:07 +0330

    feat: UoW - IApartmentRepository

---
+++ Domain/Apartments/IApartmentRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Apartments
+{
+    internal interface IApartmentRepository
+    {
+        Task<Apartment?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    }
+}
